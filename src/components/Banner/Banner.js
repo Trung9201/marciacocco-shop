1b72cbc4447f0ca8eecb00c3b478a2ab63d3a5fe
Nguyễn Quốc Trung <94846543+Trung9201@users.noreply.github.com>
2022-12-19 16:46:27 +0700

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = (props) => {
   return (
     <div className="banner">
       <img className="pic" src={props.src} alt={props.alt} />
-      <img className="cloud" src="/images/cloud-light.png" alt="cloud" />
+      <img className="cloud" src="images/cloud-light.png" alt="cloud" />
       <div className="title">
         <h1>{props.title}</h1>
         <h3>{props.text}</h3>
